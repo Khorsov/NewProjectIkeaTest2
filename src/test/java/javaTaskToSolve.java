545a7914f7833fea3025b21afd9162068990d7b8
Khorsov <hhorsovv@gmail.com>
2022-02-07 18:13:08 -0800

    Tasks commit final + java task

---
+++ src/test/java/javaTaskToSolve.java
@@ -0,0 +1,19 @@
+public class javaTaskToSolve {
+    public static  String initialsOfName(String name){
+        String initialsOfName = "";
+        for(int i=0;i<=name.length()-1;i++){
+            char character = name.charAt(i);
+            int ascii = (int) character;
+            if(ascii>=65&&ascii<=90){   //using ASKII table numbers
+                String ww = new Character((char) ascii).toString();
+                initialsOfName+=ww+".";
+            }
+        }
+        return initialsOfName;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(initialsOfName("Bruno Mars Yo"));
+        System.out.println(initialsOfName("Bruno Mars"));
+    }
+}
